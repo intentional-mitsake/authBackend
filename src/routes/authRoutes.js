2e7b9c1ca1dc346a45e5dbc7aff5c5e74422a9d4
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-07-11 11:14:17 +0545

    wiring to routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,18 +1,18 @@
 import express from 'express'
 import { reqValidator } from '../middleware/ authValidator.js'
 import { regCredVerification, logCredVerification } from '../controllers/authController.js'
-import { login_limiter, reg_limiter, logout_limiter } from '../middleware/rate_limiter.js'
+import { ratelimiter } from '../middleware/rate_limiter.js'
 import { tokenVerification } from '../middleware/authMiddleware.js'
 import { logout } from '../services/authServices.js'
 const router = express.Router()
 
 //registration
-router.post('/register', reqValidator, reg_limiter, regCredVerification)
+router.post('/register', reqValidator, ratelimiter, regCredVerification)
 
 //login
-router.post('/login', reqValidator, login_limiter,  logCredVerification)
+router.post('/login', reqValidator, ratelimiter,  logCredVerification)
 
 //logout
-router.post('/logout', logout_limiter, tokenVerification,  logout)
+router.post('/logout', ratelimiter, tokenVerification,  logout)
 
 export default router
