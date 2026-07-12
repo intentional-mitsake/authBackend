4e8ccb22499a8fd791dc6bf8da3fd448b8ef9daf
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-07-12 11:13:06 +0545

    audit logs schema

---
+++ prisma/migrations/20260712052743_add_audit_log/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "AuditLog" (
+    "id" TEXT NOT NULL,
+    "userId" INTEGER,
+    "action" TEXT NOT NULL,
+    "data" JSONB,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "AuditLog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "AuditLog_userId_idx" ON "AuditLog"("userId");
+
+-- CreateIndex
+CREATE INDEX "AuditLog_createdAt_idx" ON "AuditLog"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "AuditLog_action_idx" ON "AuditLog"("action");
