8eb64dbd0a816b20e1cb187dc4544be19c9cebc2
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-07-18 09:41:41 +0545

    fix user initializaiton

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,8 +4,7 @@ import bcrypt from "bcryptjs";
 const prisma = new PrismaClient();
 
 async function main() {
-    pw = 'Admin123!';
-    hashedpw = await bcrypt.hash(pw, 10);
+    const hashedpw = await bcrypt.hash('Admin123!', 10);
     const admin = await prisma.user.create({
         data: {
             username: 'admin',
